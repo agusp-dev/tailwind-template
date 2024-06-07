105e6ca8880aef044ede292c895581406ffd5705
Agustin <perezp.agustin@gmail.com>
2024-06-06 21:36:31 -0300

    remove user button from header - resolve dark mode button issues

--- src/components/Toolbar/index.tsx
+++ src/components/Toolbar/index.tsx
@@ -1,6 +1,4 @@
-// import { MenuButton } from '~/components/MenuButton'
 import { DarkModeButton } from '~/components/DarkModeButton'
-import { UserButton } from '~/components/UserButon'
 import { HeaderNav } from '~/components/HeaderNav'
 import { Brand } from '~/components/Brand'
 
@@ -10,11 +8,7 @@ export const Toolbar = () => {
       <div className='h-16 max-w-6xl flex justify-between items-center mx-auto'>
         <Brand />
         <HeaderNav />
-        {/* <strong className='text-xl text-purple-700'>Tailwind Template</strong> */}
-        <div className='flex items-center gap-1'>
-          <DarkModeButton />
-          <UserButton />
-        </div>
+        <DarkModeButton />
       </div>
     </header>
   )
